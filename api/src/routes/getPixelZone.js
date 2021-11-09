41beff8c519e4b259a1700c38a36df876417a4ef
CoDaTi <kevin@utocat.com>
2021-11-09 20:24:40 +0000

    feat: add more params to get pixels zone

--- api/src/routes/getPixelZone.js
+++ api/src/routes/getPixelZone.js
@@ -10,23 +10,60 @@ Object.assign(module.exports, {
   regex: /^\/pixels\/zone\/$/,
   method: "GET",
   returnString: true,
-  schema: Joi.object({
-    x: Joi.number().integer().positive().required(),
-    y: Joi.number().integer().positive().required(),
-    r: Joi.number().integer().required(),
-  }),
+  schema:
+  Joi.alternatives().try(
+    Joi.object({
+      x: Joi.number().integer().positive().required(),
+      y: Joi.number().integer().positive().required(),
+      r: Joi.number().integer().positive().required(),
+    }),
+    Joi.object({
+      minx: Joi.number().integer().positive().required(),
+      miny: Joi.number().integer().positive().required(),
+      maxx: Joi.number().integer().positive().required(),
+      maxy: Joi.number().integer().positive().required(),
+    }),
+    Joi.object({
+      departement: Joi.string().required(),
+    })
+  ),
 
   handler: async (url, data) => {
-    console.time();
+    let pixels
+    if(data.x && data.y)
+    {
+      pixels = await models["Pixels"].find(
+        {
+          x: {  $gte: data.x - data.r,$lte: data.x + data.r},
+          y: {  $gte: data.y - data.r,$lte: data.y + data.r },
+        },
+        { _id: false }
+      );
+    }else if(data.minx && data.miny && data.maxx && data.maxy )
+    {
+      pixels = await models["Pixels"].find(
+        {
+          x: { $gte: data.minx, $lte: data.maxx },
+          y: { $gte: data.miny, $lte: data.maxy},
+        },
+        { _id: false }
+      );
+    }else if(data.departement){
+      const [departement] =await models["Departements"].find({name:data.departement},{min:true,max:true, _id: false});
+      if(!departement){
+        throw Error("not found");
+        return;
+      }
 
-    let pixels = await models["Pixels"].find(
-      {
-        x: { $lte: data.x + data.r, $gte: data.x - data.r },
-        y: { $lte: data.y + data.r, $gte: data.y - data.r },
-      },
-      { _id: false }
-    );
-    console.timeLog();
+      console.log(departement)
+      pixels=await models["Pixels"].find(
+        {
+          x: { $gte: departement.min.x, $lte: departement.max.x},
+          y: { $gte: departement.min.y, $lte: departement.max.y},
+        },
+        { _id: false }
+      );
+    }
 
     let pixelsString = [];
     for (var i = 0; i < pixels.length; i++) {
@@ -42,7 +79,6 @@ Object.assign(module.exports, {
         }}`
       );
     }
-    console.timeEnd();
 
     return `[${pixelsString.toString()}]`;
   },

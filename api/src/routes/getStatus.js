94644d23889da4cd31f5933bc3845ff604135db2
CoDaTi <kevin@utocat.com>
2021-11-10 11:57:48 +0000

    fix: rm console.log

--- api/src/routes/getStatus.js
+++ api/src/routes/getStatus.js
@@ -7,8 +7,6 @@ Object.assign(module.exports, {
   method: "GET",
   handler: async (url, data) => {
     const test = await client.get("isUp");
-
-    console.log(test)
     return {
       mongodb:(await mongoose()).connection.readyState === 1,
       redis:test==="true",

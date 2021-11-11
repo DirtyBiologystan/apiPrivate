4ebdbaa5a785bd94ac2d696b537b983aff475d8b
CoDaTi <kevin@utocat.com>
2021-11-11 20:52:07 +0000

    fix: lint

--- api/src/services/convert.js
+++ api/src/services/convert.js
@@ -4,15 +4,11 @@ Object.assign(module.exports, {
       pixel.indexInFlag
     },"index":${pixel.index},"hexColor":"${pixel.hexColor}","author":"${
       pixel.author
-    }"${
-      pixel.pseudo ? `,"pseudo":${JSON.stringify(pixel.pseudo)}` : ""
-    }}`,
+    }"${pixel.pseudo ? `,"pseudo":${JSON.stringify(pixel.pseudo)}` : ""}}`,
   pixelDépartementsToJSON: (pixel) =>
     `{"x":${pixel.x},"y":${pixel.y},"indexInFlag":${
       pixel.indexInFlag
     },"index":${pixel.index},"hexColor":"${pixel.hexColor}","author":"${
       pixel.author
-    }"${
-      pixel.pseudo ? `,"pseudo":${JSON.stringify(pixel.pseudo)}` : ""
-    }}`,
+    }"${pixel.pseudo ? `,"pseudo":${JSON.stringify(pixel.pseudo)}` : ""}}`,
 });

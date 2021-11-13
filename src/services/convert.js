289ff0c80e1dc83067bcbc05919741a94acfcb3a
CoDaTi <kevin@utocat.com>
2021-11-13 04:21:30 +0000

    feat: return discord info

--- src/services/convert.js
+++ src/services/convert.js
@@ -4,12 +4,12 @@ Object.assign(module.exports, {
       pixel.indexInFlag
     },"index":${pixel.index},"hexColor":"${pixel.hexColor}","author":"${
       pixel.author
-    }"${pixel.pseudo ? `,"pseudo":${JSON.stringify(pixel.pseudo)}` : ""}}`,
+    }"${pixel.pseudo ? `,"pseudo":${JSON.stringify(pixel.pseudo)}` : ""}${pixel.discord ?`,"discord":{"pseudo":${JSON.stringify(pixel.discord.pseudo)},"id":"${pixel.discord.id}"}`:""}`,
   pixelDépartementsToJSON: (pixel) =>
     `{"x":${pixel.x},"y":${pixel.y},"indexInFlag":${
       pixel.indexInFlag
     },"index":${pixel.index},"hexColor":"${pixel.hexColor}","author":"${
       pixel.author
-    }"${pixel.pseudo ? `,"pseudo":${JSON.stringify(pixel.pseudo)}` : ""},
+    }"${pixel.pseudo ? `,"pseudo":${JSON.stringify(pixel.pseudo)}` : ""}${pixel.discord ?`,"discord":{"pseudo":${JSON.stringify(pixel.discord.pseudo)},"id":"${pixel.discord.id}"}`:""},
      "departements":${JSON.stringify(pixel.departements)}}`,
 });
